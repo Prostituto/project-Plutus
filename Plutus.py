2727195d83fa172a999a41af38692fcaf1ddf52e
Marc Peejay Viernes <quantum.01.official@gmail.com>
2020-07-17 12:14:03 +0800

    Initialize the Logic class and attemp to integrate it with the API and GUI class.

--- Plutus.py
+++ Plutus.py
@@ -12,13 +12,13 @@ from iqoptionapi.stable_api import IQ_Option
 from accounts import test_accounts
 
 
-class Logic():
+class API():
     def __init__(self):
         # Initialize the IQ Option API
         self.__Plutus = IQ_Option(test_accounts["email_1"], test_accounts["password_1"])
 
-        # Utility Variables -----------------------------------------------------------------------------------------------------------
-        self.__iq_option_api_version = self.get_iq_option_api_version()
+        # Utility Variables -----------------------------------------------------------------------------------------------------------        
+        self.__iq_option_api_version = IQ_Option.__version__
         self.__server_timestamp = 0
 
         # Connection Status
@@ -38,20 +38,21 @@ class Logic():
 
         self.__Plutus.set_session(self.__header, self.__cookie)
 
-        response = self.connect_to_server()
-        self.__is_connected = response["is_connected"]
-        self.__connection_status_reason = response["connection_status_reason"]
+        server_response = self.connect_to_server()
+        self.set_is_connected(server_response)
+        self.set_connection_status_reason(server_response)
+        
 
-        if self.__is_connected:
+        if self.get_is_connected():
             print("######### [ Success ] >> First Connection Attempt to the iq option Server #########")
 
-            self.__server_timestamp = self.get_server_timestamp()
+            self.set_server_timestamp_online()
 
             # Global Variables --------------------------------------------------------------------------------------------------------
-            # All User Information Plus Balances and Others
-            self.__acct_data = self.get_acct_data_online()
+            # All User Information Plus Balances and Other Data
+            self.__acct_data = self.__Plutus.get_profile_ansyc()
 
-            # User Information
+            # User Information Data
             self.__id = self.__acct_data["id"]
             self.__name = self.__acct_data["name"]
             self.__nickname = self.__acct_data["nickname"]
@@ -62,26 +63,23 @@ class Logic():
             self.__email = self.__acct_data["email"]
             self.__phone = self.__acct_data["phone"]
             
-            # Other/All Account Balances Data
-            self.__balances = self.__acct_data["balances"]
-            self.__real_balance = self.__balances[0]
-            self.__practice_balance = self.__balances[1]
+            # All the Different Balance Type Data
+            self.__balances_data = self.__acct_data["balances"]
+            self.__real_balance_data = self.__balances_data[0]
+            self.__practice_balance_data = self.__balances_data[1]
 
-            # Current Account Balance Data
-            self.__balance = self.__acct_data["balance"]
-            self.__balance_id = self.__acct_data["balance_id"]
-            self.__balance_type = self.__acct_data["balance_type"]
-            self.__currency = self.__acct_data["currency"]
-            self.__currency_char = self.__acct_data["currency_char"]
+            # Current Basic Balance Type Data
+            self.__current_balance = self.__acct_data["balance"]
+            self.__current_balance_id = self.__acct_data["balance_id"]
+            self.__current_balance_type = self.__acct_data["balance_type"]
+            self.__current_currency = self.__acct_data["currency"]
+            self.__current_currency_char = self.__acct_data["currency_char"]
             # End of: Global Variables ------------------------------------------------------------------------------------------------
         else:
             print("######### [ FAIL ] >> First Connection Attempt to the iq option Server #########")
             print("######### [ REASON ] >>", self.__connection_status_reason, "#########")
     
-    # Utility Functions
-    def get_iq_option_api_version(self):
-        return IQ_Option.__version__
-
+    # Utility Functions ---------------------------------------------------------------------------------------------------------------
     def connect_to_server(self):
         is_connected, connection_status_reason =  self.__Plutus.connect()
 
@@ -92,16 +90,69 @@ class Logic():
 
     def check_connection(self):
         return self.__Plutus.check_connect()
+    # End of: Utility Functions -------------------------------------------------------------------------------------------------------
+
+    # Getter Functions ----------------------------------------------------------------------------------------------------------------
+    # Get the Utility Variables
+    def get_plutus(self):
+        return self.__Plutus
+    
+    def get_iq_option_api_version(self):
+        return self.__iq_option_api_version
 
     def get_server_timestamp(self):
-        return self.__Plutus.get_server_timestamp()
+        return self.__server_timestamp
+    
+    def get_is_connected(self):
+        return self.__is_connected
 
-    # Getter Functions
-    def get_acct_data_online(self):
-        return self.__Plutus.get_profile_ansyc()
+    def get_connection_status_reason(self):
+        return self.__connection_status_reason
 
-    def get_balances_data_online(self):
-        return self.__Plutus.get_balances()
+    def get_header(self):
+        return self.__header
+    
+    def get_cookie(self):
+        return self__cookie
+
+    def get_error_password(self):
+        self.__error_password
+
+    # Get All of the User Infotmation Plus Balances and Other Data
+    def get_acct_data(self):
+        return self.__acct_data
+
+    # Get User Information Data
+    def get_id(self):
+        return self.__id
+
+    def get_name(self):
+        return self.__name
+
+    def get_nickname(self):
+        return self.__nickname
+
+    def get_gender(self):
+        return self.__gender
+
+    def get_birthdate(self):
+        return self.__birthdate
+
+    def get_nationality(self):
+        return self.__nationality
+
+    def get_address(self):
+        return self.__address
+
+    def get_email(self):
+        return self.__email
+
+    def get_phone(self):
+        return self.__phone
+    
+    # Get All the Different Balance Type Data
+    def get_balances_data(self):
+        return self.__balances
 
     def get_real_balance_data(self, balances):
         return balances[0]
@@ -109,23 +160,133 @@ class Logic():
     def get_practice_balance_data(self, balances):
         return balances[1]
 
-    # Setter Functions
+    # Get the Current Basic Balance Type Data
+    def get_current_balance(self):
+        return self.__current_balance
+
+    def get_current_balance_id(self):
+        return self.__current_balance_id
+
+    def get_current_balance_type(self):
+        return self.__current_balance_type
+
+    def ger_current_currency(self):
+        return self.__current_currency
+
+    def get_current_currency_char(self):
+        return self.__current_currency_char
+    # End of: Getter Functions --------------------------------------------------------------------------------------------------------
+
+    # Setter Functions ----------------------------------------------------------------------------------------------------------------
+    # Set the Utility Variables
+    def set_iq_option_api(self, email, password):
+        self.__Plutus = IQ_Option(email, password)
+
+    def set_iq_option_api_version(self):
+        self.__iq_option_api_version = IQ_Option.__version__
+
+    def set_server_timestamp_online(self):
+        try:
+            self.__server_timestamp = self.__Plutus.get_server_timestamp()
+        except:
+            print("######### [ ERROR ] >> Cannot Retrieve Server Timestamp #########")
+
+            self.__server_timestamp = 0
+
+    def set_is_connected(self, server_response):
+        self.__is_connected = server_response["is_connected"]
+    
+    def set_connection_status_reason(self, server_response):
+
+        self.__connection_status_reason = server_response["is_connected"]
+
+    def set_header(self, new_header):
+        self.__header = new_header
+
+    def set_cookie(self, new_cookie):
+        self.__cookie = new_cookie
+
+    def set_error_password(self, new_error_password):
+        self.__error_password = new_error_password
+
+    # Set All of the User Information Plus Balances and Other Data
+    def set_acct_data_online(self):
+        self.__acct_data = self.__Plutus.get_profile_ansyc()
+
+    # Set User Information Data
+    def set_id(self, acct_data):
+        self.__id = acct_data["id"]
+    
+    def set_name(self, acct_data):
+        self.__name = acct_data["name"]
+
+    def set_nickname(self, acct_data):
+        self.__nickname = acct_data["nickname"]
+
+    def set_gender(self, acct_data):
+        self.__gender = acct_data["gender"]
+    
+    def set_birthdate(self, acct_data):
+        self.__birthdate = acct_data["birthdate"]
+
+    def set_nationality(self, acct_data):
+        self.__nationality = acct_data["nationality"]
+
+    def set_address(self, acct_data):
+        self.__address = acct_data["address"]
+
+    def set_email(self, acct_data):
+        self.__email = acct_data["email"]
+    
+    def set_phone(self, acct_data):
+        self.__phone = acct_data["phone"]
+
+    # Set All the Different Balance Type Data
+    def set_balances_data_online(self):
+        self.__balances_data = self.__Plutus.get_profile_ansyc()
+
+    def set_real_balance_data(self, balances_data):
+        self.__real_balance_data = balances_data[0]
+
+    def set_practice_balance_data(self, balances_data):
+        self.__practice_balance_data = balances_data[1]
+
+    # Set the Current Basic Balance Type Data
+    def set_current_balance(self, acct_data):
+        self.__current_balance = acct_data["balance"]
+
+    def set_current_balance_id(self, acct_data):
+        self.__current_balance_id = acct_data["balance_id"]
+    
+    def set_current_balance_type(self, acct_data):
+        self.__current_balance_type = acct_data["balance_type"]
+        
+    def set_current_currency_char(self, acct_data):
+        self.__current_currency_char = acct_data["currency_char"]
+    # End of: Setter Functions ----------------------------------------------------------------------------------------------------------------
 
 
-class Bridge():
+class Logic():
     def __init__(self):
-        gui = GUI()
-        logic = Logic()
+        app = QApplication(sys.argv)
+        self.gui = GUI()
+
+        self.api = API()
 
         self.initialize_gui_values()
 
+        sys.exit(app.exec_())
+
     def initialize_gui_values(self):
+        self.gui.set_current_balance(self.api.get_current_balance())
+        self.gui.set_current_currency_type(self.api.get_current_currency_char())
         pass
 
 
 class GUI(QWidget):
     def __init__(self):
         super().__init__()
+        
         self.initialize_gui()
 
         # Global Variables ------------------------------------------------------------------------------------------------------------
@@ -138,41 +299,41 @@ class GUI(QWidget):
         # =============================================================================================================================
         # Python GUI Setup Using PyQt5
         # =============================================================================================================================
-        __grid_account_info = QGridLayout()
-        __grid_account_info.setSpacing(10)
+        __grid_acct_info = QGridLayout()
+        __grid_acct_info.setSpacing(10)
 
         # Account Balance
-        __label_balance = QLabel("Balance:")
-        __grid_account_info.addWidget(__label_balance, 0, 0)
+        __lbl_current_balance = QLabel("Balance:")
+        __grid_acct_info.addWidget(__lbl_current_balance, 0, 0)
 
-        self.__label_balance_value = QLabel()
-        __grid_account_info.addWidget(self.__label_balance_value, 0, 1)
+        self.__lbl_current_balance_value = QLabel()
+        __grid_acct_info.addWidget(self.__lbl_current_balance_value, 0, 1)
 
         # Currency Type
-        __label_currency_type = QLabel("Currency Type:")
-        __grid_account_info.addWidget(__label_currency_type, 1, 0)
+        __lbl_current_currency_type = QLabel("Currency Type:")
+        __grid_acct_info.addWidget(__lbl_current_currency_type, 1, 0)
 
-        self.__label_currency_type_value = QLabel(self)
-        __grid_account_info.addWidget(self.__label_currency_type_value, 1, 1)
+        self.__lbl_current_currency_type_value = QLabel(self)
+        __grid_acct_info.addWidget(self.__lbl_current_currency_type_value, 1, 1)
         
         # Reset Balance Button
-        __button_reset_balance = QPushButton("Reset Balance")
+        __btn_reset_balance = QPushButton("Reset Balance")
         # button_reset_balance.clicked.connect(self.reset_balance)
 
         # Refresh Button
-        __button_refresh = QPushButton("Refresh")
+        __btn_refresh = QPushButton("Refresh")
         # button_refresh.clicked.connect(self.refresh_everything)
 
         # Group Box - Account Informaiton
-        __group_box_account_info = QGroupBox("Account Information")
-        __group_box_account_info.setCheckable(False)
+        __group_box_acct_info = QGroupBox("Account Information")
+        __group_box_acct_info.setCheckable(False)
 
-        __group_box_account_info.setLayout(__grid_account_info)
+        __group_box_acct_info.setLayout(__grid_acct_info)
 
         # Grid Layout - Main
         __grid_main = QGridLayout()
         __grid_main.setSpacing(10)
-        __grid_main.addWidget(__group_box_account_info, 0, 0)
+        __grid_main.addWidget(__group_box_acct_info, 0, 0)
 
         self.setLayout(__grid_main)
         self.setGeometry(300, 300, 200, 200)
@@ -183,15 +344,15 @@ class GUI(QWidget):
         # =============================================================================================================================
 
     # Mutator Functions ---------------------------------------------------------------------------------------------------------------
-    def set_balance_gui(self, balance):
-        self.__label_balance_value.setText(str(balance))
-        self.__label_balance_value.adjustSize()
+    def set_current_balance(self, current_balance):
+        self.__lbl_current_balance_value.setText(str(current_balance))
+        self.__lbl_current_balance_value.adjustSize()
 
         return True
 
-    def set_currency_type_gui(self, currency_type):
-        self.__label_currency_type_value.setText(currency_type)
-        self.__label_currency_type_value.adjustSize()
+    def set_current_currency_type(self, current_currency_type):
+        self.__lbl_current_currency_type_value.setText(current_currency_type)
+        self.__lbl_current_currency_type_value.adjustSize()
 
         return True
     # End of: Mutator Funcions --------------------------------------------------------------------------------------------------------
@@ -220,13 +381,8 @@ def main():
                         # datefmt="%H:%M:%S",
                         level=logging.DEBUG)
 
-    Plutus = Logic()
-
     # Pyhton GUI Initialization
-    app = QApplication(sys.argv)
-    gui = GUI()
-
-    sys.exit(app.exec_())
+    logic = Logic()
 
 
 if __name__ == "__main__":
